b844150fa3247bad65070ce3028a2ae1d96da5f6
andreffs18 <andreffs18@gmail.com>
2018-10-14 22:48:45 +0100

    Refactor users endpoint

--- project/__init__.py
+++ project/__init__.py
@@ -72,12 +72,12 @@ def create_app(config=None):
     from project.admin.models import ModelView
     from project.user.models import User
     admin = Admin(app, name='Admin Panel', template_mode='bootstrap3')
-    admin.add_view(ModelView(User, db.session, basic_auth, endpoint="admin/user"))
+    admin.add_view(ModelView(User, db.session, basic_auth, endpoint="users"))
 
     # register api endpoints
     from project.api.v1.user import User
     api = Api(app, prefix='/api/v1/')
-    api.add_resource(User, 'user/', endpoint='api.v1.user')
+    api.add_resource(User, 'users/', endpoint='api.v1.users')
 
     # import custom login manager functions
     from project.user.login_manager import load_user_from_request, load_user
